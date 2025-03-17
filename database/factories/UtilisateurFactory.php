ece9f7772e566b3d0719b35885a3c0622ecb6f48
petitho91 <petitho91@gmail.com>
2025-03-17 00:09:27 +0000

    Création de la factory Utilisateur

---
+++ database/factories/UtilisateurFactory.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Role;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Utilisateur>
+ */
+class UtilisateurFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            "nom" => fake()->lastName(),
+            "prenom" => fake()->firstName(),
+            "email" => fake()->email(),
+            "telephone" => fake()->phoneNumber(),
+            "password" => "passer123",
+            "role_id" => fake()->randomElement(Role::pluck('id'))
+        ];
+    }
+}
